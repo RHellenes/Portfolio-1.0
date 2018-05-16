23ebc114dc507e6970890afdd50e9e10805f201b
René Hellenes <rene.hellenes@gmail.com>
2018-05-16 18:26:31 +0200

    About page  - finished(ish)
    The about page is basically finished, but it need the footer and a typo-check

--- om_meg.php
+++ om_meg.php
@@ -5,10 +5,67 @@
 	include_once($toRoot.'header.php');
 ?>
 
+<main>
+
+	<h1>Om Meg</h1>
+
+	<article class="half">
+
+		<div class="section">
+			<div class="title">Navn:</div>
+			<div class="content">René Hellenes</div>
+		</div>
+
+		<div class="section">
+			<div class="title">Alder:</div>
+			<div class="content">23 år</div>
+		</div>
+
+		<div class="section">
+			<div class="title">Utdanning:</div>
+			<div class="content">Bachelor i Visuell Kommunikasjon ved Høgskolen i Sør- Øst Norge avd. Drammen</div>
+		</div>
+
+		<div class="section">
+			<div class="title">Jobbstatus:</div>
+			<div class="content">Søker</div>
+		</div>
+
+		<div class="section">
+			<div class="title">Stikkord:</div>
+			<div class="content">
+				<ul>
+					<li>Front-end utvikler</li>
+					<li>Designer</li>
+					<li>Strategisk</li>
+					<li>Lærelysten</li>
+				</ul>
+			</div>
+		</div>
+	</article>
 
 
 
+<article class="full">
 
+		<div class="section">
+			<div class="title">Mer om meg:</div>
+			<div class="content">Jeg ønsker meg en arbeidsplass som har et ambisiøst og et sterkt faglig miljø, med kolleger som har stolthet i arbeidet sitt og ønsker å lærebort det de vet! Samtidig som de setter pris på hyggelige samtaler i pausen, om faglige, så vell som emner som opptar dem på fritiden.
+
+			Min kompetanse strekker seg hovedsaklig over emnene; strategisk design, markedsføring, konseptutvikling, webdesign og front end utvikling.</div>
+		</div>
+
+
+
+		<div class="section">
+			<div class="title">Perfekte jobb:</div>
+			<div class="content">Jeg ønsker meg en arbeidsplass som har et ambisiøst og et sterkt faglig miljø, med kolleger som har stolthet i arbeidet sitt og ønsker å lære bort det de kan! Samtidig som de setter pris på hyggelige samtaler i pausen, om faglige, så vell som emner som opptar dem på fritiden.
+
+			Vet du om en ledig stilling eller tenker at jeg ville passet inn i teamet ditt eller til noen du kjenner? TIPS MEG!&#x1F603;</div>
+		</div>
+
+</article>
+</main>
 
 
 
